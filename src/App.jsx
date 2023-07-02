b182ccace0e43ae0ca29b21658c03433e055e612
debanjo31 <debanjo31@gmail.com>
2023-07-02 16:10:04 +0100

    major commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Map from "./Component/Map";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <p className="text-center py-4 bg-blue-800 text-white">
+        Cloud Integration Project
+      </p>
+      <Map />
+    </>
+  );
+}
+
+export default App;
